76acb1424a5e5b7750949c7bb4ff228451d287af
snxlno <144980968+snxlno@users.noreply.github.com>
2023-09-21 23:51:23 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import random
 st.title("study with you")
 
 if st.button("let's study !!"):
-    results = ["15minutes","30minutes","1hour","1.5hours","2hours","breaktime"]
+    results = ["15minutes","30minutes","1hour","1.5hours","2hours","breaktime !"]
     result = random.choice(results)
     st.write(f"結果:{result}")
