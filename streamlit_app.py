f4ac5e5bbd825b7b2a9dcc4d56244b52eaf65d9e
snxlno <144980968+snxlno@users.noreply.github.com>
2023-09-22 00:09:54 +0000

    ｱ

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ if st.button("let's study !!"):
     result = random.choice(results)
     st.write(f"結果:{result}")
 
- subject = random.choice(["英語", "数学", "プログラミング", "歴史", "地理", "国語", "理科", "社会", "その他"])
+    subject = random.choice(["英語", "数学", "プログラミング", "歴史", "地理", "国語", "理科", "社会", "その他"])
     place = random.choice(["自宅", "図書館", "カフェ", "公園", "学校", "その他"])
     st.write(f"結果:{result}")
     st.write(f"勉強する内容:{subject}")
